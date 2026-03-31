b8658cb3c155e7e179a8e082eeff8750c7eb5b14
Luigi Arena <luigiarena.cs@gmail.com>
2026-03-31 17:57:41 +0200

    Add localStorage to store theme viarable in ThemeContext component

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -3,7 +3,28 @@ import React, { Children, createContext, useState, useEffect } from 'react'
 const ThemeContext = createContext();
 
 function ThemeProvider({ children }) {
-  const [darkMode, setDarkMode] = useState(false);
+  function saveToLocalStorage(state) {
+    try {
+      const serialisedState = JSON.stringify(state);
+      localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+  function loadFromLocalStorage() {
+    try {
+      const serialisedState = localStorage.getItem("persistantState");
+      if (serialisedState === null) return undefined;
+      return JSON.parse(serialisedState);
+    } catch (e) {
+      console.warn(e);
+      return undefined;
+    }
+  }
+  const persistedState = loadFromLocalStorage();
+
+  const [darkMode, setDarkMode] = useState(persistedState?.darkMode || false);
 
   const toggleThemes = () => {
     setDarkMode(prev => !prev);
@@ -15,6 +36,7 @@ function ThemeProvider({ children }) {
     } else {
       document.getElementById('root').classList.remove('dark');
     }
+    saveToLocalStorage({ darkMode });
   }, [darkMode]);
 
   return (
