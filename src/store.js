78821112d9501e236334945c64853491682fcbc6
Luigi Arena <luigiarena.cs@gmail.com>
2026-04-01 18:11:13 +0200

    Add Redux store with a test slice

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import testSlice from "./features/test/testSlice";
+
+const store = configureStore({
+    reducer: {
+        test: testSlice,
+    }
+})
+
+export default store;
